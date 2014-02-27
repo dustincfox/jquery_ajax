7dbd6c1107ec65318ee1f851f4756627376915b9
Dustin Fox <dustincfox@gmail.com>
2014-02-27 09:34:51 -0600

    final code

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,13 @@
 $(document).ready(function(){
 
+  $("#get_color").on("click", function(event){
+    event.preventDefault();
+
+    $.post('/color', function(serverResponse){
+      data = JSON.parse(serverResponse);
+      var box = "#color_me :nth-child(" + data.cell + ")";
+      $(box).css("backgroundColor", data.color);
+    })
+  })
+
 });
